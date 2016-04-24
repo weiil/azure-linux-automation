0bd32be57afdf40ee78874abb5b6810610b4c99b
weiil <v-lii@microsoft.com>
2016-04-24 16:24:02 +0800

    add test script for bosh cf deployment

---
+++ remote-scripts/Template-bosh-cf-deployment.ps1
@@ -0,0 +1,116 @@
+<#-------------Create Deployment Start------------------#>
+Import-Module .\TestLibs\RDFELibs.psm1 -Force
+$result = ""
+$testResult = ""
+$resultArr = @()
+
+try
+{
+    $templateName = $currentTestData.testName
+    $parameters = $currentTestData.parameters
+    $location = $xmlConfig.config.Azure.General.Location
+
+    if(Test-Path .\azuredeploy.parameters.json)
+    {
+        Remove-Item .\azuredeploy.parameters.json
+    }
+
+    # update template parameter file 
+    LogMsg 'update template parameter file '
+    $jsonfile =  Get-Content .\azure-quickstart-templates\bosh-setup\azuredeploy.parameters.json -Raw | ConvertFrom-Json
+    $curtime = Get-Date
+    $timestr = "-" + $curtime.Month + "-" +  $curtime.Day  + "-" + $curtime.Hour + "-" + $curtime.Minute + "-" + $curtime.Second
+    $jsonfile.parameters.vmName.value = $parameters.vmName + $timestr
+    $jsonfile.parameters.adminUsername.value = $parameters.adminUsername
+    $jsonfile.parameters.sshKeyData.value = $parameters.sshKeyData
+    $jsonfile.parameters.environment.value = $parameters.environment
+    $jsonfile.parameters.tenantID.value = $parameters.tenantID
+    $jsonfile.parameters.clientID.value = $parameters.clientID
+    $jsonfile.parameters.clientSecret.value = $parameters.clientSecret
+    $jsonfile.parameters.autoDeployBosh.value = $parameters.autoDeployBosh
+    
+    # save template parameter file
+    $jsonfile | ConvertTo-Json | Out-File .\azuredeploy.parameters.json
+    if(Test-Path .\azuredeploy.parameters.json)
+    {
+        LogMsg "successful save azuredeploy.parameters.json"
+    }
+    else
+    {
+        LogMsg "fail to save azuredeploy.parameters.json"
+    }
+
+
+    $isDeployed = CreateAllRGDeploymentsWithTempParameters -templateName $templateName -location $location -TemplateFile .\azure-quickstart-templates\bosh-setup\azuredeploy.json  -TemplateParameterFile .\azuredeploy.parameters.json
+
+    if ($isDeployed[0] -eq $True)
+    {
+
+        $testResult_deploy_bosh = "PASS"
+    }
+    else
+    {
+        $testResult_deploy_bosh = "Failed"
+    }
+
+    # connect to the devbox then deploy multi-vms cf
+    $dep_ssh_info = $(Get-AzureResourceGroupDeployment -ResourceGroupName $isDeployed[1]).outputs['sshDevBox'].Value.Split(' ')[1]
+    $port = 22
+    $sshKey = "cf_devbox_privatekey.ppk"
+    $command = 'hostname'
+    
+    # ssh to devbox and deploy multi-vms cf
+    echo y | .\tools\plink -i .\ssh\$sshKey -P $port $dep_ssh_info "$command"
+    $out = echo y | .\tools\plink -i .\ssh\$sshKey -P $port $dep_ssh_info "./deploy_cloudfoundry.sh example_manifests/multiple-vm-cf.yml && echo multi_vms_cf_deploy_ok || echo multi_vms_cf_deploy_fail"
+
+    if ($out -match "multi_vms_cf_deploy_ok")
+    {
+        $testResult_deploy_multi_vms_cf = "PASS"
+    }
+    else
+    {
+        $testResult_deploy_multi_vms_cf = "Failed"
+    }
+
+    if ($testResult_deploy_bosh -eq "PASS" -and $testResult_deploy_multi_vms_cf -eq "PASS")
+    {
+        $testResult = "PASS"
+    }
+    else
+    {
+        $testResult = "Failed"
+    }
+
+
+    $testStatus = "TestCompleted"
+    LogMsg "Test result : $testResult"
+
+    if ($testStatus -eq "TestCompleted")
+    {
+        LogMsg "Test Completed"
+    }
+
+
+}
+catch
+{
+    $ErrorMessage =  $_.Exception.Message
+    LogMsg "EXCEPTION : $ErrorMessage"   
+}
+Finally
+{
+    $metaData = ""
+    if (!$testResult)
+    {
+        $testResult = "Aborted"
+    }
+    $resultArr += $testResult
+}
+
+$result = GetFinalResultHeader -resultarr $resultArr
+
+#Clean up the setup
+DoTestCleanUp -result $result -testName $currentTestData.testName -deployedServices $isDeployed[1] -ResourceGroups $isDeployed[1]
+
+#Return the result and summery to the test suite script..
+return $result
